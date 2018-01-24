7988fd590ae6fff07eda5febb83a98ac2334bbfb
Pear <mpearcey.59024@farnborough.ac.uk>
2018-01-24 14:10:20 +0000

    Added some buttons

--- src/Frame.java
+++ src/Frame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Frame extends JFrame {
 
@@ -11,6 +13,7 @@ public class Frame extends JFrame {
         setContentPane(new JLayeredPane());
         addBackground();
         addCanvas();
+        addToolBar();
     }
 
     private void addBackground() {
@@ -20,12 +23,35 @@ public class Frame extends JFrame {
         bg.setOpaque(true);
         getContentPane().add(bg, new Integer(0));
     }
+
     private void addCanvas() {
         JLabel drawCanvas = new JLabel();
-        drawCanvas.setBounds(20, 20, 500, 520);
+        drawCanvas.setBounds(20, 20, 600, 520);
         drawCanvas.setLocation(20, 20);
         drawCanvas.setBackground(Color.WHITE);
         drawCanvas.setOpaque(true);
         getContentPane().add(drawCanvas, new Integer(1));
+    }
+
+    private void addToolBar() {
+        Color colors[] = {Color.BLACK, Color.WHITE, Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN, Color.PINK, Color.CYAN, Color.MAGENTA, Color.ORANGE};
+        final int beginX = 700, beginY = 20;
+        int colorIndex = 0;
+        for(int y = 0; y < 5; y++) {
+            for(int x = 0; x < 2; x++) {
+                JLabel Chooser = new JLabel();
+                Chooser.setBounds(beginX + (x*100), beginY + (y*100), 80, 80);
+                Chooser.setBackground(colors[colorIndex]);
+                Chooser.setOpaque(true);
+                Chooser.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        System.out.println("e = [" + e + "]");
+                    }
+                });
+                getContentPane().add(Chooser, new Integer(1));
+                colorIndex++;
+            }
+        }
     }
 }
